4f04b87cd1a74b80913a9cb0996e00f12d0b211e
yunusy <yunusy@student.uia.no>
2021-09-06 22:55:23 +0200

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elmasi";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklma = "amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklma = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            //type-safe --tip guvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklma);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("................");
+            }
+
+            Console.WriteLine(".............Metotlar----------");
+            SepetMenager sepetMenager = new SepetMenager();
+            sepetMenager.Ekle(urun1);
+            sepetMenager.Ekle(urun2);
+
+            sepetMenager.Ekle2("Armut", "Yesil Armut", 12, 10);
+            sepetMenager.Ekle2("Karpuz", "Yesil Karpuz", 20, 18);
+            sepetMenager.Ekle2("Elma", "Yesil Elma", 24, 23);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ classintro/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "EnginDemirog";
+            kurs1.izlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "EnginDemirog";
+            kurs2.izlenmeOrani = 60;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Pyton";
+            kurs3.Egitmen = "EnginDemirog";
+            kurs3.izlenmeOrani = 55;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "EnginDemirog";
+            kurs4.izlenmeOrani = 75; 
+
+            Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+
+            {
+                Console.WriteLine(kurs.KursAdi+  " "+ kurs.Egitmen+ " "+ kurs.izlenmeOrani);
+            }
+
+            Console.WriteLine("Sayfasonu--footer");
+            Console.ReadKey();
+
+        }
+    }
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+        
+    
+
+    }
+    
+
+}
+
+
+
+
