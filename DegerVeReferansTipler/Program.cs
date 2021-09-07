38622a35daee4dff93b2b1c1e4274256ef50d989
yunusy <yunusy@student.uia.no>
2021-09-07 22:53:20 +0200

    20 ocak derssonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -21,7 +21,7 @@ namespace DegerVeReferansTipler
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]);
-
+           
 
             Console.ReadKey();
         }
